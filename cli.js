e8fe9fc8750dd68e6cd0f839559560b6af73b91f
Nishan <nishan.bende@solutelabs.com>
2019-01-22 08:35:45 +0530

    Spinner and set/reset credentials

--- cli.js
+++ cli.js
@@ -1,43 +1,55 @@
 #! /usr/bin/env node
-const fs = require("fs");
+
 const prompts = require("prompts");
+const fs = require("fs");
 const opn = require("opn");
 const projects = JSON.parse(fs.readFileSync("projects.json"));
+const args = process.argv;
 const {
-    generateAuthHeader,
-    getAxiosInstance,
-    getJIRADateFormat,
-    requiredValidator,
-    getWorkLogPromptFields,
-    getStatusPrompt
+  generateAuthHeader,
+  getAxiosInstance,
+  getJIRADateFormat,
+  requiredValidator,
+  getWorkLogPromptFields,
+  getStatusPrompt,
+  spinner
 } = require("./utils");
-const main = async () => {
-    try {
-        const axiosInstance = await getAxiosInstance();
-        const {
-            ISSUE_KEY,
-            TIME_SPENT,
-            COMMENT
-        } = await getWorkLogPromptFields();
-        let baseurl = projects[ISSUE_KEY.split("-")[0]]["url"];
-        const url = `${baseurl}/rest/api/2/`;
-        axiosInstance.defaults.baseURL = url;
-        const response = await axiosInstance.post(
-            `/issue/${ISSUE_KEY}/worklog`,
-            {
-                started: getJIRADateFormat(),
-                timeSpent: TIME_SPENT,
-                comment: COMMENT || `Working on issue ${ISSUE_KEY}`
-            }
-        );
-        console.log("Worklog has been added");
-        const { STATUS } = await getStatusPrompt();
-        if (STATUS) {
-            opn(`${baseurl}/browse/${ISSUE_KEY}`);
-        }
-    } catch (error) {
-        console.log("Failed Status code ", error.response.status);
-    }
-};
 
-main();
+const main = async () => {
+  try {
+    const axiosInstance = await getAxiosInstance();
+    const { ISSUE_KEY, TIME_SPENT, COMMENT } = await getWorkLogPromptFields();
+    let baseurl = projects[ISSUE_KEY.split("-")[0]]["url"];
+    const url = `${baseurl}/rest/api/2/`;
+    axiosInstance.defaults.baseURL = url;
+
+    spinner.start();
+
+    const response = await axiosInstance.post(`/issue/${ISSUE_KEY}/worklog`, {
+      started: getJIRADateFormat(),
+      timeSpent: TIME_SPENT,
+      comment: COMMENT || `Working on issue ${ISSUE_KEY}`
+    });
+
+    spinner.stop();
+
+    console.log("Worklog has been added");
+    const { STATUS } = await getStatusPrompt();
+    if (STATUS) {
+      opn(`${baseurl}/browse/${ISSUE_KEY}`);
+    }
+  } catch (error) {
+    spinner.stop();
+    if (error.response) {
+      console.log("Failed Status code ", error.response.status);
+    } else {
+      console.log("Cyaa...");
+    }
+  }
+};
+const userFlag = args[2];
+if (userFlag && userFlag.includes("cred")) {
+  require("./credentials.js");
+} else {
+  main();
+}
---
+++ credentials.js
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+const fs = require("fs");
+const prompts = require("prompts");
+const {
+  ENV_FILE_PATH,
+  initializeAPIToken,
+  initializeEmail
+} = require("./utils");
+
+fs.writeFile(ENV_FILE_PATH, "", async function() {
+  await initializeEmail();
+  await initializeAPIToken();
+});
